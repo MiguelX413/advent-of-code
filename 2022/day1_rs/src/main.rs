05a0ecfb41f641ac27bcbbe6aa79d43c50c39171
MiguelX413 <lbpgaming27@gmail.com>
2022-12-03 00:40:10 -0800

    Move file contents into own var

--- 2022/day1_rs/src/main.rs
+++ 2022/day1_rs/src/main.rs
@@ -4,8 +4,8 @@ use std::{env, fs};
 fn main() {
     let args = env::args().collect::<Vec<String>>();
     let file_path = Path::new(&args[1]);
-    let max = fs::read_to_string(file_path)
-        .unwrap()
+    let file_contents = fs::read_to_string(file_path).unwrap();
+    let max = file_contents
         .split("\n\n")
         .map(|f| {
             f.split_whitespace()
